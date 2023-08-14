5ca9e158ae60f3de32082cf145e811c1965f3be1
vlcoscar8 <vlcoscar8@github.com>
2023-08-14 14:08:51 +0200

    Ejercicio 3: dotenv con fs sin dependencias

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // reference: https://www.npmjs.com/package/dotenv
+const fs = require('node:fs')
 
 function config (options = {}) {
-  // your code here
+  const fileExist = fs.existsSync(options.path || '.env')
+
+  if (fileExist) {
+    const envFileContent = fs.readFileSync(options.path || '.env', 'utf-8')
+
+    envFileContent.split('\n').forEach(env => {
+      const envArr = env.split('=')
+
+      const envName = envArr[0]
+      const envValue = envArr[1].replaceAll('"','')
+
+      process.env[envName] = envValue
+    })
+  }
 }
 
 module.exports = { config }
