444d6f7a2bebbefc5afefcf26d409f334d76252e
vlcoscar8 <vlcoscar8@github.com>
2023-08-14 14:20:07 +0200

    Refactor: const path creada para evitar repetir codigo y destructuracion del env.split

--- index.js
+++ index.js
@@ -2,18 +2,16 @@
 const fs = require('node:fs')
 
 function config (options = {}) {
-  const fileExist = fs.existsSync(options.path || '.env')
+  const path = options.path || '.env'
+  const fileExist = fs.existsSync(path)
 
   if (fileExist) {
-    const envFileContent = fs.readFileSync(options.path || '.env', 'utf-8')
+    const envFileContent = fs.readFileSync(path, 'utf-8')
 
     envFileContent.split('\n').forEach(env => {
-      const envArr = env.split('=')
+      const [key, value] = env.split('=')
 
-      const envName = envArr[0]
-      const envValue = envArr[1].replaceAll('"','')
-
-      process.env[envName] = envValue
+      process.env[key] = value.replaceAll('"','')
     })
   }
 }
